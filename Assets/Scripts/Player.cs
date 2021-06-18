a794d8c8c700d3070d50725f58180299340aedff
Camrynlangdon <44718126+Camrynlangdon@users.noreply.github.com>
2021-06-18 15:38:06 -0700

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    int playerSpeed;
+
+    [SerializeField]
+    int jumpHeight;
+
+    private new Rigidbody2D rigidbody;
+    private bool playerIsTouchingGround;
+    private int playerHasJumped = 0;
+
+    void Update()
+    {
+        if (Input.GetKey("d"))
+            movePlayerXY(1);
+        if (Input.GetKey("a"))
+            movePlayerXY(-1);
+        if (Input.GetKeyDown(KeyCode.Space))
+            playerJump();
+        Debug.Log("is player touching ground?" + playerIsTouchingGround);
+    }
+
+    public void movePlayerXY(int direction)
+    {
+        transform.position += new Vector3(direction * Time.deltaTime * playerSpeed, 0, 0);
+    }
+
+    public void playerJump()
+    {
+        if (playerHasJumped <= 1)
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+            rigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            playerHasJumped += 1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerIsTouchingGround = true;
+        playerHasJumped = 0;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        playerIsTouchingGround = false;
+    }
+}
