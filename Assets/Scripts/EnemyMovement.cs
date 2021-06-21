6d5fe016ac4c06a1e7afab29b41eca5650f28452
Camrynlangdon <44718126+Camrynlangdon@users.noreply.github.com>
2021-06-21 02:34:26 -0700

    added ememy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -11,6 +11,9 @@ public class EnemyMovement : MonoBehaviour
     public Vector2 positionToMoveTo;
 
     [SerializeField]
+    bool delayedStart = false;
+
+    [SerializeField]
     float timeToMove;
 
     [SerializeField]
@@ -22,53 +25,107 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     float resetTimeToMove = 0;
 
+    [SerializeField]
+    string action;
+
+    [SerializeField]
+    string idle;
+
+    [SerializeField]
+    string[] randomEvent;
+
     public GameObject gameObjectToMove;
+    private bool isRunning;
+    private int timesMoved;
+    private Vector2 startPosition;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Vector2 startPosition = gameObjectToMove.transform.localPosition;
-        StartCoroutine(LerpPosition(positionToMoveTo, startPosition, timeToMove));
-        StartCoroutine(delayedAnimationPlay("Hit", timeToMove * .5f));
-        StartCoroutine(delayedResetObjectPoition(startPosition, timeToMove * .5f + timeAfterAnimationForReset));
+        if (!isRunning)
+        {
+
+
+
+            if (timesMoved == 0)
+            {
+                StartCoroutine(LerpPosition(startPosition, positionToMoveTo, timeToMove));
+                startPosition = gameObjectToMove.transform.localPosition;
+
+            }
+
+
+
+
+        }
+
+
     }
 
     private void triggerAnimation(string triggerName)
     {
+        if (action == null || action == "") return;
         Animator animator = gameObjectToMove.GetComponent<Animator>();
         animator.SetTrigger(triggerName);
     }
 
+    private void triggerAnimationLoopStop(string triggerName)
+    {
+
+        if (string.IsNullOrEmpty(idle)) return;
+        Animator animator = gameObjectToMove.GetComponent<Animator>();
+        animator.SetTrigger(triggerName);
+    }
 
     private IEnumerator delayedAnimationPlay(string triggerName, float time)
     {
         yield return new WaitForSeconds(time);
         triggerAnimation(triggerName);
-        Debug.Log("delay");
     }
 
-    private IEnumerator delayedResetObjectPoition(Vector2 targetPosition, float duratuion)
-    {
-        yield return new WaitForSeconds(duratuion);
-        StartCoroutine(LerpPosition(targetPosition, positionToMoveTo, resetTimeToMove));
-
-    }
-    private IEnumerator LerpPosition(Vector2 targetPosition, Vector2 startPosition, float duration)
+    private IEnumerator LerpPosition(Vector2 startPosition, Vector2 targetPosition, float duration)
     {
         float time = 0;
+        timesMoved++;
+        isRunning = true;
+
+
+        if (timesMoved == 1)
+        {
+            if (!delayedStart)
+                triggerAnimation(action);
+            else
+                StartCoroutine(delayedAnimationPlay(action, timeToMove * .5f));
+        }
+
 
 
         while (time < duration)
         {
             float t = time / duration;
-            t = t * t * (5f + 5f * t);
+            t = t * t * (1f + 10f * t);
 
             gameObjectToMove.transform.localPosition = Vector2.Lerp(startPosition, targetPosition, t);
             time += Time.deltaTime;
             yield return null;
         }
         gameObjectToMove.transform.localPosition = targetPosition;
+
+
+        isRunning = false;
+        resetPosition();
+        if (targetPosition == this.startPosition)
+        {
+            timesMoved = 0;
+            triggerAnimationLoopStop(idle);
+        }
     }
 
+
+    private void resetPosition()
+    {
+        if (resetObjectAfterAnimation && timesMoved == 1)
+            StartCoroutine(LerpPosition(positionToMoveTo, startPosition, resetTimeToMove));
+    }
 
 }
