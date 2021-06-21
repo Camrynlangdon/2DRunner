be6b2170998700bf54bc820efb2d286d690b6eef
Camrynlangdon <44718126+Camrynlangdon@users.noreply.github.com>
2021-06-20 20:40:10 -0700

    cleaned Hierarchy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+
 
     [SerializeField]
     public Vector2 positionToMoveTo;
@@ -11,30 +13,62 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     float timeToMove;
 
+    [SerializeField]
+    bool resetObjectAfterAnimation = false;
+
+    [SerializeField]
+    float timeAfterAnimationForReset = 0;
+
+    [SerializeField]
+    float resetTimeToMove = 0;
+
     public GameObject gameObjectToMove;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        StartCoroutine(LerpPosition(positionToMoveTo, timeToMove));
-        Debug.Log("attack player!");
+        Vector2 startPosition = gameObjectToMove.transform.localPosition;
+        StartCoroutine(LerpPosition(positionToMoveTo, startPosition, timeToMove));
+        StartCoroutine(delayedAnimationPlay("Hit", timeToMove * .5f));
+        StartCoroutine(delayedResetObjectPoition(startPosition, timeToMove * .5f + timeAfterAnimationForReset));
+    }
+
+    private void triggerAnimation(string triggerName)
+    {
+        Animator animator = gameObjectToMove.GetComponent<Animator>();
+        animator.SetTrigger(triggerName);
     }
 
 
-    IEnumerator LerpPosition(Vector2 targetPosition, float duration)
+    private IEnumerator delayedAnimationPlay(string triggerName, float time)
+    {
+        yield return new WaitForSeconds(time);
+        triggerAnimation(triggerName);
+        Debug.Log("delay");
+    }
+
+    private IEnumerator delayedResetObjectPoition(Vector2 targetPosition, float duratuion)
+    {
+        yield return new WaitForSeconds(duratuion);
+        StartCoroutine(LerpPosition(targetPosition, positionToMoveTo, timeToMove));
+
+    }
+    private IEnumerator LerpPosition(Vector2 targetPosition, Vector2 startPosition, float duration)
     {
         float time = 0;
-        Vector2 startPosition = gameObjectToMove.transform.localPosition;
+
 
         while (time < duration)
         {
-
             float t = time / duration;
-            t = t * t * (3f - 2f * t);
-            gameObjectToMove.transform.localPosition = Vector2.Lerp(startPosition, targetPosition, t);
+            t = t * t * (5f + 5f * t);
 
+            gameObjectToMove.transform.localPosition = Vector2.Lerp(startPosition, targetPosition, t);
             time += Time.deltaTime;
             yield return null;
         }
         gameObjectToMove.transform.localPosition = targetPosition;
     }
+
+
 }
