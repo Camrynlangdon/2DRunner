4d05f9ea90c8038e7bc87eabe557f5200563271b
Camrynlangdon <44718126+Camrynlangdon@users.noreply.github.com>
2021-06-22 06:42:48 -0700

    seprated item movement from item animation

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -1,8 +1,41 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
+    public GameObject gameObjectToAnimate;
 
+
+    public void playRandomAnimation(string[] randomAnimation)
+
+    {
+        if (randomAnimation.Length == 0) return;
+        int number = 1;
+        int randomNum = Random.Range(0, 10);
+        if (randomNum == number)//play animation
+        {
+            int randomIndex = Random.Range(0, randomAnimation.Length);
+            int randomAnimationTriggerDelay = Random.Range(0, 10);
+            delayedAnimationPlay(randomAnimation[randomIndex], randomAnimationTriggerDelay);
+        }
+    }
+    public void triggerAnimation(string triggerName)
+    {
+        if (string.IsNullOrEmpty(triggerName)) return;
+        Animator animator = gameObjectToAnimate.GetComponent<Animator>();
+        animator.SetTrigger(triggerName);
+    }
+
+    public void triggerAnimationLoopStop(string triggerName)
+    {
+        if (string.IsNullOrEmpty(triggerName)) return;
+        Animator animator = gameObjectToAnimate.GetComponent<Animator>();
+        animator.SetTrigger(triggerName);
+    }
+
+    public IEnumerator delayedAnimationPlay(string triggerName, float time)
+    {
+        yield return new WaitForSeconds(time);
+        triggerAnimation(triggerName);
+    }
 }
