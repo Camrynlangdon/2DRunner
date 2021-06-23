605d414144b296bae822ad5a1623c6658e5cd9a4
davon <davonwhitworth@gmail.com>
2021-06-22 17:33:50 -0700

    added reset on trophy and r button

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+
+    public GameController gameController;
+    private int delay = 2; 
+
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(DelayedRestart());
+    }
+
+    IEnumerator DelayedRestart()
+    {
+        yield return new WaitForSeconds(delay);
+        gameController.resetCurrentLevel();
+    }
+}
